c3dbe787f2c6700587b7d283a84fda0e0aab5218
PatrickReis32 <rogerfumassa9@gmail.com>
2026-03-13 20:36:56 -0300

    Create Ex20.c

---
+++ Exercicios/Ex20.c
@@ -0,0 +1,18 @@
+//EX20
+
+#include<stdio.h>
+	int main(){
+		int a, b, x;
+			printf("Escreva o valor de A: ");
+				scanf("%d", &a);
+					printf("\nEscreva o valor de B: ");
+						scanf("%d", &b);
+							printf("\nVoce digitou a= %d, b= %d", a, b);
+								x = a;
+								a = b;
+								b = x;
+									printf("\nO novo valor trocado vai ser:\na= %d, b= %d", a , b); 
+						
+						
+		
+	}
