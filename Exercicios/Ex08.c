cf6f68e87354ba2fcef1d2379b0357158e33ba72
Vitor Patrick <rogerfumassa9@gmail.com>
2026-03-15 17:58:21 -0300

    Create Ex08.c

---
+++ Exercicios/Ex08.c
@@ -0,0 +1,13 @@
+//Ex08
+//Calcular 20% de um valor qualquer.
+
+#include<stdio.h>
+	int main(){
+		float a, r;
+			printf("O valor que vai ser calculado: ");
+				scanf("%f", &a);
+					r = a * 0.2;	
+						printf("\no valor de 20% de %0.2f, eh de %0.2f", a, r);
+				
+	}
+	
