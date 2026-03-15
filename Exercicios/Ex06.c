3f504a68c75d99088e815d26de06865efafb8cf2
Vitor Patrick <rogerfumassa9@gmail.com>
2026-03-15 17:36:34 -0300

    Create Ex06.c

---
+++ Exercicios/Ex06.c
@@ -0,0 +1,10 @@
+//Ex06
+//Criar um algoritmo que imprima a média aritmética entre os números 8, 9 e 7.
+
+#include<stdio.h>
+	int main(){
+		int a=8, b=9, c=7;
+		float media;
+			media = (a + b + c) / 3;
+				printf("O valor da media eh: %2.0f", media);
+	}
