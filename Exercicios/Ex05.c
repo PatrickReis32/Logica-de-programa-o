7cb574edcbeb499494fa93db82fee8c05c2ec751
Vitor Patrick <rogerfumassa9@gmail.com>
2026-03-15 17:12:46 -0300

    Create Ex05.c

---
+++ Exercicios/Ex05.c
@@ -0,0 +1,13 @@
+//Ex05
+//Efetuar a soma de dois números e apresentar o resultado.
+
+#include<stdio.h>
+	int main(){
+		float a, b, r;
+			printf("Digite o primeiro numero:");
+				scanf("%f",&a);
+					printf("Digite o segundo numero: ");
+						scanf("%f", &b);
+							r = a + b;
+								printf("\nO resultado da soma eh: %2.0f", r);
+	}
